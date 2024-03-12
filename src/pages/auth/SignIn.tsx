60002c29dff485126b4d232b20f9d1672739df8d
Vicente <vikom.sanchez@gmail.com>
2024-03-12 13:23:37 -0300

    feat(pages/sign-in): add sign in form

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,10 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center ">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu email</Label>
+              <Input type="email" id="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
