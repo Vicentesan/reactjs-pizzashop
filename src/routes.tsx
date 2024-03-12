e319211a6872d5ebffc1b40359775d5032280958
Vicente <vikom.sanchez@gmail.com>
2024-03-12 15:22:38 -0300

    feat(pages/sign-up): add sign up page following the template of the sign in page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Dashboard } from './pages/app/Dashboard'
 import { SignIn } from './pages/auth/SignIn'
+import { SignUp } from './pages/auth/SignUp'
 import { AppLayout } from './pages/layouts/app'
 import { AuthLayout } from './pages/layouts/auth'
 
@@ -23,6 +24,10 @@ export const router = createBrowserRouter([
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
