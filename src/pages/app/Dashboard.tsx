fffa805b18b1820df113fa5a48f82a84a2f627a4
Vicente <vikom.sanchez@gmail.com>
2024-03-13 15:33:06 -0300

    feat(pages/dashboard): add initial dashboard component

--- src/pages/app/Dashboard.tsx
+++ src/pages/app/Dashboard.tsx
@@ -1,10 +1,24 @@
 import { Helmet } from 'react-helmet-async'
 
+import { DayOrdersAmountCard } from '@/components/DayOrdersAmountCard'
+import { MonthCanceledOrdersAmountCard } from '@/components/MonthCanceledOrdersAmountCard'
+import { MonthOrdersAmountCard } from '@/components/MonthOrdersAmountCard'
+import { MonthRevenueCard } from '@/components/MonthRevenueCard'
+
 export function Dashboard() {
   return (
     <>
       <Helmet title="Painel" />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
+      </div>
     </>
   )
 }
