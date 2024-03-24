8030127bb7d8350ff19a556b0f9f721e13aebc0d
Vicente <vikom.sanchez@gmail.com>
2024-03-24 15:09:33 -0300

    feat(pages/dashboard): add month orders amount data from API

--- src/components/MonthOrdersAmountCard.tsx
+++ src/components/MonthOrdersAmountCard.tsx
@@ -1,8 +1,17 @@
+import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
+
+import { getMonthsOrdersAmount } from '@/api/get-month-orders-amount'
+import { cn } from '@/lib/utils'
 
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 
 export function MonthOrdersAmountCard() {
+  const { data: monthOrdersAmount } = useQuery({
+    queryKey: ['metrics', 'month-orders-amount'],
+    queryFn: getMonthsOrdersAmount,
+  })
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,11 +20,31 @@ export function MonthOrdersAmountCard() {
       </CardHeader>
 
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">246</span>
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+6%</span> em
-          relação ao mês passado
-        </p>
+        {monthOrdersAmount && (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {monthOrdersAmount.amount.toLocaleString('pt-BR')}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              <span
+                className={cn('', {
+                  'text-rose-500 dark:text-rose-400':
+                    monthOrdersAmount.diffFromLastMonth < 0,
+                  'text-emerald-500 dark:text-emerald-400':
+                    monthOrdersAmount.diffFromLastMonth > 0,
+                })}
+              >
+                {monthOrdersAmount.diffFromLastMonth === 0
+                  ? monthOrdersAmount.diffFromLastMonth
+                  : monthOrdersAmount.diffFromLastMonth > 0
+                    ? `+${monthOrdersAmount.diffFromLastMonth}`
+                    : `${monthOrdersAmount.diffFromLastMonth}`}
+                %
+              </span>{' '}
+              em relação ao mês passado
+            </p>
+          </>
+        )}
       </CardContent>
     </Card>
   )
