0b05fa1a9a348187ebc3ba6f2642dfacbf0a4bb6
Vicente <vikom.sanchez@gmail.com>
2024-04-03 16:34:25 -0300

    test(e2e): add get-month-orders-amount mock api

--- src/api/get-month-orders-amount.ts
+++ src/api/get-month-orders-amount.ts
@@ -1,6 +1,6 @@
 import { api } from '@/lib/axios'
 
-interface GetMonthsOrdersAmountResponse {
+export interface GetMonthsOrdersAmountResponse {
   amount: number
   diffFromLastMonth: number
 }
---
+++ src/api/msw/get-month-orders-amount-mock.ts
@@ -0,0 +1,14 @@
+import { http, HttpResponse } from 'msw'
+
+import { GetMonthsOrdersAmountResponse } from '../get-month-orders-amount'
+
+export const getMonthOrdersAmountMock = http.get<
+  never,
+  never,
+  GetMonthsOrdersAmountResponse
+>('/metrics/month-orders-amount', () => {
+  return HttpResponse.json({
+    amount: 20,
+    diffFromLastMonth: -5,
+  })
+})
