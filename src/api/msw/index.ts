83a49a6724f65191004120d73da5f2a64f6b5b32
Vicente <vikom.sanchez@gmail.com>
2024-04-03 16:56:39 -0300

    test(e2e): add get-popular-products mock api

--- src/api/msw/index.ts
+++ src/api/msw/index.ts
@@ -7,6 +7,7 @@ import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
 import { signInMock } from './sign-in-mock'
 
@@ -18,6 +19,7 @@ export const worker = setupWorker(
   getMonthCanceledOrdersAmountMock,
   getMonthRevenueMock,
   getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
 )
 
 export async function enableMSW() {
