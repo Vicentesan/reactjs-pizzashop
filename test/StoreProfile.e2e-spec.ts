785cbf5783503c74e97d255188e679dfef364c84
Vicente <vikom.sanchez@gmail.com>
2024-04-13 12:43:25 -0300

    test(e2e): add store profile e2e test

---
+++ test/StoreProfile.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { expect, test } from '@playwright/test'
+
+import { mockedRestaurantName } from './utils'
+
+test('update profile successfully', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: mockedRestaurantName }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Pizza Shop')
+  await page.getByLabel('Descrição').fill('Another Description')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await page.waitForTimeout(250)
+
+  expect(page.getByRole('button', { name: 'Pizza Shop' })).toBeVisible()
+})
